9505b689f380abb1394aa72200ff124405664d1c
Summer <85640068+haiibo@users.noreply.github.com>
2023-11-07 16:01:58 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -73,11 +73,11 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-ssr-mudb-ser
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-pppoe-server
 svn export https://github.com/281677160/openwrt-package/trunk/luci-app-cpulimit
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-webdav/luci-app-gowebdav
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-webdav/gowebdav
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox/luci-app-homebox
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox/homebox
-svn export https://github.com/syb999/openwrt-19.07.1/trunk/package/network/services/msd_lite && mv -n msd_lite/luci-app-msd_lite ./
+svn export https://github.com/281677160/openwrt-package/trunk/luci-app-msd_lite
+svn export https://github.com/xiangfeidexiaohuo/extra-ipk/trunk/op-webdav/luci-app-gowebdav
+svn export https://github.com/xiangfeidexiaohuo/extra-ipk/trunk/op-webdav/gowebdav
+svn export https://github.com/xiangfeidexiaohuo/extra-ipk/trunk/op-homebox/luci-app-homebox
+svn export https://github.com/xiangfeidexiaohuo/extra-ipk/trunk/op-homebox/homebox
 
 git_sparse_clone openwrt-18.06 "https://github.com/immortalwrt/luci" "luci" applications/luci-app-eqos \
 applications/luci-app-aliddns applications/luci-app-gost applications/luci-app-iptvhelper
